8749c3c55d7a10065a6f3e9c8f86979681fcd750
Patrick <pa@arthurfamily.ws>
2015-09-14 00:41:23 -0400

    python class for bank customers

---
+++ bank_customer.py
@@ -0,0 +1,43 @@
+class BankCustomer(object):
+    """customer of a Bank with a checking account. Customers have the
+    following:
+
+    Attributes:
+        name: A string representing the customer's name.
+        balance: A float tracking the current balance of the customer's account.
+    """
+
+    def __init__(self, name):
+        """Return a Customer object whose name is *name* and starting
+        balance is 0."""
+        self.name = name
+        self.balance = 0.0
+
+    def set_balance(self, balance):
+        """Set the customer's starting balance."""
+        self.balance = balance
+        return str(self.name) + ' initiial balance is ' + str(self.balance) + ' dollars'
+
+    def withdraw(self, amount):
+        """Return the balance remaining after withdrawing *amount*
+        dollars."""
+        if amount > self.balance:
+            raise RuntimeError('Amount greater than available balance.')
+        self.balance -= amount
+        return str(self.name) + ' withdrew ' + str(self.balance) + ' dollars'
+
+    def deposit(self, amount):
+        """Return the balance remaining after depositing *amount*
+        dollars."""
+        self.balance += amount
+        return str(self.name) + ' deposited ' + str(self.balance) + ' dollars'
+
+x = BankCustomer("Patrick Arthur")
+print x.set_balance(120)
+print x.withdraw(50)
+print x.deposit(10)
+
+y = BankCustomer("Romulus Superior")
+print y.set_balance(1000)
+print y.withdraw(10)
+print y.deposit(100)
